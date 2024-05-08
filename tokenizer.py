e98f64dd245a232831e11955cb8ec58e825614a5
Farhan Zaman <farhanz@circinus-31.ics.uci.edu>
2024-05-07 22:33:16 -0700

    implemented a tokenizer that keeps hyphenated words and words with apostrophes together as single tokens

---
+++ tokenizer.py
@@ -0,0 +1,24 @@
+from nltk.tokenize import RegexpTokenizer
+
+
+def tokenize(parsed_text: str) -> list:
+    """
+    Parameters:
+    parsed_text -- a string object of plain text
+    
+    Return:
+    a list of all the words in the string. Words that are hyphenated or that have apostrophes in them are counted as single tokens.
+    """
+
+    # regex that tokenizes hyphenated words or words with apostrophes as singular tokens
+    pattern = r"\b(?:[a-zA-Z]+(?:'[a-zA-Z]+)?(?:-[a-zA-Z]+)?)\b"
+    # Create a tokenizer using the custom pattern
+    tokenizer = RegexpTokenizer(pattern)
+    # Tokenize the given text
+    tokens_list = tokenizer.tokenize(parsed_text)
+    return tokens_list
+
+
+if __name__ == '__main__':
+    pass
+    # print( tokenize("I can't believe it's May 7th, and we're still waiting for the sun to shine. asdf-qwer zxcv/tyui") )
