3165fb4b7185d269d619c821cbcc8aa6c6db5584
farhanzam <104218893+farhanzam@users.noreply.github.com>
2024-05-07 22:40:34 -0700

    Update tokenizer.py
    small changes to comments

--- tokenizer.py
+++ tokenizer.py
@@ -3,14 +3,14 @@ from nltk.tokenize import RegexpTokenizer
 
 def tokenize(parsed_text: str) -> list:
     """
-    Parameters:
-    parsed_text -- a string object of plain text
+    Parameters
+    parsed_text: a string object of plain text
     
-    Return:
-    a list of all the words in the string. Words that are hyphenated or that have apostrophes in them are counted as single tokens.
+    Return
+    tokens_list: a list of all the words in the string. Words that are hyphenated or that have apostrophes in them are counted as single tokens.
     """
 
-    # regex that tokenizes hyphenated words or words with apostrophes as singular tokens
+    # regex pattern that splits the string into separate word-tokens
     pattern = r"\b(?:[a-zA-Z]+(?:'[a-zA-Z]+)?(?:-[a-zA-Z]+)?)\b"
     # Create a tokenizer using the custom pattern
     tokenizer = RegexpTokenizer(pattern)
