4fea379dd4ca6493e37cef1a11f950f50397d8d3
farhanzam <104218893+farhanzam@users.noreply.github.com>
2024-05-09 17:23:02 -0700

    Tokenizer (#2)
    * implemented a tokenizer that keeps hyphenated words and words with apostrophes together as single tokens
    
    * Update tokenizer.py
    
    small changes to comments
    
    * minor additions to parse.py: skipping '.DS_Store' files; stripping leading & trailing whitespace. Implemented Porter stemming to tokenizer
    
    * Update parser.py
    
    * Delete parser.py
    
    name of this file conflicts with some 'parser' module from nltk, so I created a new file named 'parse.py'
    
    * updated tokenizer to update dictionaries of word frequencies, started working on postings.py, created a globals.py file to hold variables we will need to access across our different files
    
    * minor change: imported globals.py
    
    * added same-page detection, and added a way to get and assign a docID to each page visited
    
    * debugging
    
    * Update tokenizer.py
    
    commenting print statement used for debugging
    
    ---------
    
    Co-authored-by: Farhan Zaman <farhanz@circinus-31.ics.uci.edu>
    Co-authored-by: Farhan Zaman <farhanz@circinus-28.ics.uci.edu>

---
+++ parse.py
@@ -0,0 +1,38 @@
+import os
+import re
+import json
+import globals
+from bs4 import BeautifulSoup as BS
+
+def build_index(directories: str) -> None:
+    """
+    Given a path to a root directory, iterate over every file in every subdirectory to get each page's plain text contents
+
+    Parameter(s)
+    directories: str object, path to the root dir
+
+    Return
+    None
+    """
+    
+    index = {}
+    ID = 0
+    for root, dirs, files in os.walk(directories):  # traverse dirs & get files at all levels
+        for file in files:  # iterate over every file in the directory
+            path = os.path.join(root, file)
+
+            if ".DS_Store" in path:  # this file is hidden in the dir, and isn't a json file; skip it if found
+                continue
+
+            js_file = open(path)  # open the file
+            js_data = json.load(js_file)  # get file contents
+            js_file.close()  # close the file
+
+            html_content = js_data['content']  # get the html content from the json dictionary
+            page_text = BS(html_content, 'html.parser').get_text(strip=True)  # get plain text
+            page_text = page_text.lower()
+
+
+if __name__ == '__main__':
+    # build_index( "/home/farhanz/CS_121/Assignment_3/searchengine121/ANALYST" )
+    pass
---
+++ tokenizer.py
@@ -0,0 +1,128 @@
+import globals
+import sqlite3
+import hashlib
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+
+
+def update_global_dict(tokens_list: list) -> None:
+    """
+    Given a list of words, add them to the global dictionary of word frequencies (this dict counts frequency of word encounters across all pages visited).
+
+    Parameter(s)
+    tokens_list: list of tokens generated from 'tokenize' function
+
+    Return
+    None
+    """
+
+    globals.FREQ_DICT
+
+    for token in tokens_list:
+        try:
+            # increment the number of times this token has been encountered by 1
+            globals.FREQ_DICT[token] += 1
+        except:
+            # if the tokene isn't in the dict, this must be the first time this token has been encountered: add the token to dict with frequency of encounters = 1
+            globals.FREQ_DICT[token] = 1
+    return
+
+
+def update_page_dict(tokens_list: list) -> dict:
+    """
+    Given a list of tokens, add them to the dictionary of tokens encountered.
+
+    Parameter(s)
+    tokens_list: list of tokens
+
+    Return
+    dictionary object of unique tokens encountered and their frequencies
+    """
+
+    freq_dict = {}
+    for token in tokens_list:
+        try:
+            # increment the number of times this token has been encountered by 1
+            freq_dict[token] += 1
+        except KeyError:
+            # if the token isn't in the dict, this must be the first time this token has been encountered: add the token to dict with frequency of encounters = 1
+            freq_dict[token] = 1
+    
+    return freq_dict
+
+
+def tokenize(parsed_text: str) -> tuple:
+    """
+    Given text in str format, split up the string into all of its constituent words. Words that are hyphenated or have apostrophes are counted as single tokens.
+
+    Parameters
+    parsed_text: a string object of plain text
+    
+    Return
+    id_and_tokens: a tuple in the form (docID, list of all the words in the string)
+    """
+    
+    # Do exact duplicate page detection
+    duplicate = check_for_duplicates(parsed_text)
+    if duplicate:  # since this page is an exact duplicate of one already visited, skip it
+        return (0, [])
+    # regex pattern: split text into separate tokens, keeping hyphenated words and words with apostrophes together
+    pattern = r"\b(?:[a-zA-Z]+(?:'[a-zA-Z]+)?(?:-[a-zA-Z]+)?)\b"
+    # Create a tokenizer using the custom pattern
+    tokenizer = RegexpTokenizer(pattern)
+    # Tokenize the given text
+    tokens_list = tokenizer.tokenize(parsed_text)
+    # create a stemmer
+    stemmer = PorterStemmer()
+    # convert all word-tokens into their stems (ex.: "dogs" -> "dog")
+    tokens_list = [stemmer.stem(word) for word in tokens_list]
+
+    globals.DOCID += 1  # this is the ith page visited: increment the counter by 1
+    return (globals.DOCID, tokens_list)
+
+
+def check_for_duplicates(text: str) -> bool:
+    """
+    check if the current page is an exact duplicate of other pages already visited. Does so by creating a sha256 hash object of the text, 
+
+    Parameters
+    text: a str object of the plain text of a page
+    url: a str object of the page's url
+
+    Return
+    a boolean object: True if this page is an exact duplicate of one already visited, False otherwise
+    """
+    
+    hash = hashlib.sha256(text.encode()).hexdigest()  # generate the sha256 hash of the given text
+    # create a connection to the database 'hashes.db' (creates the db if it doesn't already exist)
+    con = sqlite3.connect("hashes.db")
+    # Create a db cursor to execute SQL statements and fetch results from SQL queries
+    cur = con.cursor()
+    # check if the 'hashes' table already exists in the 'hashes' db
+    res = cur.execute("SELECT name FROM sqlite_master WHERE name='hashes'")
+    res = res.fetchone()  # if res == anything other than None, then it exists in db
+    if not res:  # if it doesn't exist, create the 'hashes' table
+        cur.execute(" CREATE TABLE hashes (hash STR) ")  # table created with one column named 'hash'
+        con.commit()  # commit the CREATE TABLE transaction to the db
+    
+    # check if hash already in db
+    res = cur.execute("SELECT * FROM hashes WHERE hash = ?", (hash,))
+    res = res.fetchone()  # if res == anything other than None, it was found in the table
+    if res:  # since it's in db, return True: this page is a duplicate of one already crawled over
+        cur.close()
+        con.close()
+        return True
+    # otherwise, add its hash to the table
+    cur.execute(f"""INSERT INTO hashes(hash) VALUES(?)""", (hash, ))
+    con.commit()  # commit the INSERT transaction to db
+
+    # close connections
+    cur.close()
+    con.close()
+    
+    return False
+
+
+if __name__ == '__main__':
+    # print( tokenize("i can't believe it's may 7th, and we're still waiting for the sun to shine. asdf-qwer zxcv/tyui") )
+    pass
